383ba6963d09ce11224b4dd1db226278dd39343c
Juan upb desktop <juanf.franco@upb.edu.co>
2026-09-16 15:02:38 -0500

    Configure example from the original code

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include "ofMain.h"
+#include "ofApp.h"
+
+//========================================================================
+int main( ){
+
+	//Use ofGLFWWindowSettings for more options like multi-monitor fullscreen
+	ofGLWindowSettings settings;
+	settings.setGLVersion(4, 3);
+	settings.setSize(1280, 736);
+	settings.windowMode = OF_WINDOW; //can also be OF_FULLSCREEN
+
+	auto window = ofCreateWindow(settings);
+
+	ofRunApp(window, std::make_shared<ofApp>());
+	ofRunMainLoop();
+
+}
